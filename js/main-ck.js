14d0b2e986e00a88d92ace2534db5f9b631243fd
Gaddafi <flisterz@gmail.com>
2012-09-18 21:51:31 +0800

    refactor structure for CSS to add preload animation and transition

--- js/main-ck.js
+++ js/main-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){});$(window).load(function(){$(".main_issue").removeClass("preload").addClass("loaded");$(".main_issue .desc").addClass("loaded");$(".main_issue .article").addClass("loaded")});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
- 
+
 });
 
+$(window).load(function(){
+	$(".main_issue").removeClass('preload').addClass('loaded');
+	$(".main_issue .desc").addClass('loaded');
+	$(".main_issue .article").addClass('loaded');
+});
 
